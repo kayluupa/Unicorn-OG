5bc6711f0b5b6972be8aae1c240f161fc5a8cb5a
kayluupa <132180420+kayluupa@users.noreply.github.com>
2023-11-19 20:41:35 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_z6i1buHOs',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '68oq1pmevho5jt87n3n81jpgd2',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
